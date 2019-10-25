c44a9f5d55b405e32ef34de54b924666c606e40a
Dmitry Gavrilov <31319017+the-dise@users.noreply.github.com>
2019-10-25 17:05:46 +0300

    update my site

---
+++ dmgc/29.js
@@ -0,0 +1,15 @@
+document.getElementById('but1').onclick = function but1 (){
+document.getElementById('map1').style.display = 'block';
+document.getElementById('map2').style.display = 'none'; 
+document.getElementById('map3').style.display = 'none'; 
+}
+document.getElementById('but2').onclick = function but2 (){
+document.getElementById('map1').style.display = 'none';
+document.getElementById('map2').style.display = 'block';    
+document.getElementById('map3').style.display = 'none'; 
+}
+document.getElementById('but3').onclick = function but3 (){
+document.getElementById('map1').style.display = 'none';
+document.getElementById('map2').style.display = 'none'; 
+document.getElementById('map3').style.display = 'block';    
+}
